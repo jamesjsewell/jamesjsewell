ca6c38e24c15e006caec2b0f7ff068aea1a30221
james sewell <jamesjosephsewell@gmail.com>
2017-08-22 13:07:24 -0500

    created image stack for vertical scroller

--- src/scripts/features/homePage/components/HomePageLayout.jsx
+++ src/scripts/features/homePage/components/HomePageLayout.jsx
@@ -4,14 +4,23 @@ export default class HomePageLayout extends Component {
 	render() {
 		return (
 			<div className="home_page_wrapper">
-					
 
 				<div className="home_page_header">
 
-					<img src="images/james-at-demo-day.jpg" />
+					<div id="slider1">
+						<img src="images/james-at-demo-day.jpg" />
+
+						<div id="slider2">
+							<img src="images/theironyard-64.jpg" />
+
+							<div id="slider3">
+								<img src="images/theironyard-66.jpg" />
+							</div>
+						</div>
+
+					</div>
 
 				</div>
-
 
 			</div>
 		);
