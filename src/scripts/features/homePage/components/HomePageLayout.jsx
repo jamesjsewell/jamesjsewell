05d5a8f3abde95a516dda78ea832ea879b85e950
james sewell <jamesjosephsewell@gmail.com>
2017-12-16 13:57:38 -0600

    header, proabout me, projects section layout responsive and complete for now, moving on to adding a skills section

--- src/scripts/features/homePage/components/HomePageLayout.jsx
+++ src/scripts/features/homePage/components/HomePageLayout.jsx
@@ -43,8 +43,7 @@ export default class HomePageLayout extends React.PureComponent {
 
         const lg = {
             logo: { i: "logo", x: 0, y: y, w: 2, h: 1 },
-            tagline: { i: "tagline", x: 2, y: y, w: 10, h: 1 },
-            contact: { i: "contact", x: 0, y: y + 1, w: 2, h: 2.5 },
+            contact: { i: "contact", x: 2, y: y, w: 10, h: 1 },
             aboutMe: { i: "about", x: 2, y: y + 1, w: 9, h: 2.5 },
             projectsHeader: { i: "projects", x: 0, y: y + 3.5, w: 12, h: 0.75 },
             project1: { i: "0", x: 0, y: y + 4, w: 3, h: 3 },
@@ -53,39 +52,56 @@ export default class HomePageLayout extends React.PureComponent {
             project4: { i: "3", x: 9, y: y + 4, w: 3, h: 3 }
         };
 
-        var proj = 10/4
+        var proj = 10 / 4;
         const md = {
             logo: { i: "logo", x: 0, y: y, w: 2, h: 1 },
-            tagline: { i: "tagline", x: 2, y: y, w: 8, h: 1 },
-            contact: { i: "contact", x: 0, y: y + 1, w: 2, h: 2.5 },
+            contact: { i: "contact", x: 2, y: y, w: 8, h: 1 },
             aboutMe: { i: "about", x: 2, y: y + 1, w: 8, h: 2.5 },
             projectsHeader: { i: "projects", x: 0, y: y + 3.5, w: 12, h: 0.75 },
             project1: { i: "0", x: 0, y: y + 4, w: proj, h: proj },
             project2: { i: "1", x: proj, y: y + 4, w: proj, h: proj },
-            project3: { i: "2", x: proj*2, y: y + 4, w: proj, h: proj },
-            project4: { i: "3", x: proj*3, y: y + 4, w: proj, h: proj }
+            project3: { i: "2", x: proj * 2, y: y + 4, w: proj, h: proj },
+            project4: { i: "3", x: proj * 3, y: y + 4, w: proj, h: proj }
         };
+        var proj = 6 / 2;
         const sm = {
-            logo: { i: "logo", x: 0, y: y, w: 2, h: 2 },
-            aboutMe: { i: "about", x: 2, y: y, w: 8, h: 2 },
-            contact: { i: "contact", x: 10, y: y, w: 2, h: 2 }
+            logo: { i: "logo", x: 0, y: y, w: 2, h: 1 },
+            contact: { i: "contact", x: 2, y: y, w: 4, h: 1 },
+            aboutMe: { i: "about", x: 2, y: y + 1, w: 8, h: 2.5 },
+            projectsHeader: { i: "projects", x: 0, y: y + 2, w: 12, h: 0.75 },
+            project1: { i: "0", x: 0, y: y + 3, w: proj, h: proj },
+            project2: { i: "1", x: proj, y: y + 3, w: proj, h: proj },
+            project3: { i: "2", x: 0, y: y + 4, w: proj, h: proj },
+            project4: { i: "3", x: proj, y: y + 4, w: proj, h: proj }
         };
+        var proj = 2;
         const xs = {
-            logo: { i: "logo", x: 0, y: y, w: 2, h: 2 },
-            aboutMe: { i: "about", x: 2, y: y, w: 8, h: 2 },
-            contact: { i: "contact", x: 10, y: y, w: 2, h: 2 }
+            logo: { i: "logo", x: 0, y: y, w: 2, h: 1 },
+            contact: { i: "contact", x: 2, y: y+1, w: 4, h: 1 },
+            aboutMe: { i: "about", x: 2, y: y + 2, w: 8, h: 3.5 },
+            projectsHeader: { i: "projects", x: 0, y: y + 3, w: 12, h: 0.75 },
+            project1: { i: "0", x: 0, y: y + 4, w: proj, h: proj },
+            project2: { i: "1", x: proj, y: y + 4, w: proj, h: proj },
+            project3: { i: "2", x: 0, y: y + 5, w: proj, h: proj },
+            project4: { i: "3", x: proj, y: y + 5, w: proj, h: proj }
         };
+        var proj = 2;
         const xxs = {
-            logo: { i: "logo", x: 0, y: y, w: 2, h: 2 },
-            aboutMe: { i: "about", x: 2, y: y, w: 8, h: 2 },
-            contact: { i: "contact", x: 10, y: y, w: 2, h: 2 }
+            logo: { i: "logo", x: 0, y: y, w: 1, h: 1 },
+            contact: { i: "contact", x: 2, y: y+1, w: 2, h: 1 },
+            aboutMe: { i: "about", x: 2, y: y + 2, w: 8, h: 5.5 },
+            projectsHeader: { i: "projects", x: 0, y: y + 3, w: 12, h: 0.5 },
+            project1: { i: "0", x: 0, y: y + 6, w: proj, h: proj },
+            project2: { i: "1", x: 0, y: y + 7, w: proj, h: proj },
+            project3: { i: "2", x: 0, y: y + 8, w: proj, h: proj },
+            project4: { i: "3", x: 0, y: y + 9, w: proj, h: proj }
         };
 
         var layouts = {
             lg: [
                 lg.logo,
-                lg.tagline,
                 lg.contact,
+
                 lg.aboutMe,
                 lg.projectsHeader,
                 lg.project1,
@@ -95,8 +111,8 @@ export default class HomePageLayout extends React.PureComponent {
             ],
             md: [
                 md.logo,
-                md.tagline,
                 md.contact,
+
                 md.aboutMe,
                 md.projectsHeader,
                 md.project1,
@@ -104,9 +120,39 @@ export default class HomePageLayout extends React.PureComponent {
                 md.project3,
                 md.project4
             ],
-            sm: [sm.logo, sm.aboutMe, sm.contact],
-            xs: [xs.logo, xs.aboutMe, xs.contact],
-            xxs: [xxs.logo, xxs.aboutMe, xxs.contact]
+            sm: [
+                sm.logo,
+                sm.contact,
+
+                sm.aboutMe,
+                sm.projectsHeader,
+                sm.project1,
+                sm.project2,
+                sm.project3,
+                sm.project4
+            ],
+            xs: [
+                xs.logo,
+                xs.contact,
+
+                xs.aboutMe,
+                xs.projectsHeader,
+                xs.project1,
+                xs.project2,
+                xs.project3,
+                xs.project4
+            ],
+            xxs: [
+                xxs.logo,
+                xxs.contact,
+
+                xxs.aboutMe,
+                xxs.projectsHeader,
+                xxs.project1,
+                xxs.project2,
+                xxs.project3,
+                xxs.project4
+            ]
         };
 
         return layouts;
@@ -149,19 +195,15 @@ export default class HomePageLayout extends React.PureComponent {
                 </div>
 
                 <div
-                    className="tagline"
-                    key="tagline"
-                    style={debug ? debugStyle : {}}
-                >
-                    James Joseph Sewell - Front end engineer, HTX
-                </div>
-
-                <div
                     className="contact"
                     key="contact"
                     style={debug ? debugStyle : {}}
                 >
-                    contact info
+                    <div className="contact_item"><img className="contact_img" src="images/github.svg"/></div>
+                    <div className="contact_item"><img className="contact_img" src="images/linkedin.svg"/></div>
+                    <div className="contact_item"><img className="contact_img" src="images/facebook.svg"/></div>
+                    <div className="contact_item"><img className="contact_img" src="images/twitter.svg"/></div>
+                    <div className="contact_item"><img className="contact_img" src="images/instagram.svg"/></div>
                 </div>
 
                 <div
@@ -169,7 +211,7 @@ export default class HomePageLayout extends React.PureComponent {
                     key="about"
                     style={debug ? debugStyle : {}}
                 >
-                    James was a hobbyist developer and wanted a more practical understanding of software development and web technologies. He attended The Iron Yard Houston in the spring of 2017. Along with daily instruction, he applied concepts learned in the form of daily exercises and projects. He gained experience using version control, even on team projects, and utilizes it frequently. He learned to use UNIX commands in the terminal, and bash scripting. He learned foundational concepts all the way up to developing his own web applications with cutting edge frameworks. He continues to adopt new technologies and develop personal projects.
+                    <p>James was a hobbyist developer and wanted a more practical understanding of software development and web technologies. He attended The Iron Yard Houston in the spring of 2017. Along with daily instruction, he applied concepts learned in the form of daily exercises and projects. He gained experience using version control, even on team projects, and utilizes it frequently. He learned to use UNIX commands in the terminal, and bash scripting. He learned foundational concepts all the way up to developing his own web applications with cutting edge frameworks. He continues to adopt new technologies and develop personal projects.</p>
                     {" "}
                 </div>
 
@@ -178,7 +220,7 @@ export default class HomePageLayout extends React.PureComponent {
                     className="projects_header"
                     style={debug ? debugStyle : {}}
                 >
-                    web applications engineered by James
+                    <h4>webapps engineered by James</h4>
                 </div>
 
                 <div key="0" style={debug ? debugStyle : {}}>
